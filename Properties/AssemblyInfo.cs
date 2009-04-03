5a75381a97b5c5909913bd8ae4d7c2fa4928a56f
Harry Pierson <harry.pierson@microsoft.com>
2009-04-03 09:53:38 -0700

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pygments.WLWriter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corp")]
+[assembly: AssemblyProduct("Pygments.WLWriter")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corp 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d041583a-f55c-4678-ac9e-4a0394947710")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PygmentsCodeSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using WindowsLive.Writer.Api;
+
+namespace DevHawk
+{
+    [WriterPlugin("2EC9848E-067D-4e79-BAB7-06CA927DB962", "Pygments.WLWriter",
+    Description = "Code Colorizer using Python pygments package",
+    PublisherUrl = "http://devhawk.net")]
+    [InsertableContentSource("Insert Pygmented Code", SidebarText = "Pygmented Code")]
+    public class PygmentsCodeSource : SmartContentSource
+    {
+        public override System.Windows.Forms.DialogResult CreateContent(IWin32Window dialogOwner, ISmartContent newContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SmartContentEditor CreateEditor(ISmartContentEditorSite editorSite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GeneratePublishHtml(ISmartContent content, IPublishingContext publishingContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GenerateEditorHtml(ISmartContent content, IPublishingContext publishingContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
