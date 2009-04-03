1f3b78d5b721763029edad699a9474cf13ffabfd
Harry Pierson <harry.pierson@microsoft.com>
2009-04-03 11:37:06 -0700

    added basic textbox layout code for CodeInsertForm

--- PygmentsCodeSource.py
+++ PygmentsCodeSource.py
@@ -1,11 +1,34 @@
 import clr
 clr.AddReference('System.Windows.Forms')
 
-from System.Windows.Forms import Form, DialogResult
+from System.Windows import Forms
+from System import Drawing
 
-class CodeInsertForm(Form):
+class CodeInsertForm(Forms.Form):
   def __init__(self):
-    self.Text = "Insert Code to Pygmentize"
+  
+    self.panel1 = Forms.Panel(
+      Dock = Forms.DockStyle.Bottom,
+      Size = Drawing.Size(40, 40),
+      )
+      
+    self.code_text_box = Forms.TextBox(
+      AcceptsReturn = True,
+      AcceptsTab = True,
+      Font = Drawing.Font("Consolas", 10),
+      Dock = Forms.DockStyle.Fill,
+      Multiline = True,
+      ScrollBars = Forms.ScrollBars.Both,
+      )
+
+    self.SuspendLayout()
+    self.ClientSize = Drawing.Size(640,480)
+    self.Controls.Add(self.code_text_box)
+    self.Controls.Add(self.panel1)
+    self.Text = "Insert Code to be Pygmented"
+    self.ResumeLayout(False)
+    self.PerformLayout()
+
 
 def CreateContent(dialogOwner, newContent):
   frm = CodeInsertForm()
