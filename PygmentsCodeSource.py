890512abcacb6295cb5394f72958ba64419edcb2
Harry Pierson <harry.pierson@microsoft.com>
2009-04-03 20:15:33 -0700

    added DevHawkHtmlFormatter

--- PygmentsCodeSource.py
+++ PygmentsCodeSource.py
@@ -12,7 +12,83 @@ from WindowsLive.Writer.Api import SmartContentEditor
 from pygments import highlight
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles, get_style_by_name
-from pygments.formatters import HtmlFormatter
+
+from pygments.formatter import Formatter
+
+class DevHawkHtmlFormatter(Formatter):
+  def __init__(self, **options):
+        Formatter.__init__(self, **options)
+
+        # create a dict of (start, end) tuples that wrap the
+        # value of a token so that we can use it in the format
+        # method later
+        self.styles = {}
+
+        # we iterate over the `_styles` attribute of a style item
+        # that contains the parsed style values.
+        for token, style in self.style:
+            start = end = ''
+            # a style item is a tuple in the following form:
+            # colors are readily specified in hex: 'RRGGBB'
+            if style['color']:
+                start += '<span style="color:#%s">' % style['color']
+                end = '</span>' + end
+            if style['bold']:
+                start += '<b>'
+                end = '</b>' + end
+            if style['italic']:
+                start += '<i>'
+                end = '</i>' + end
+            if style['underline']:
+                start += '<u>'
+                end = '</u>' + end
+            self.styles[token] = (start, end)
+
+
+  def format(self, tokensource, outfile):
+        # lastval is a string we use for caching
+        # because it's possible that an lexer yields a number
+        # of consecutive tokens with the same token type.
+        # to minimize the size of the generated html markup we
+        # try to join the values of same-type tokens here
+        lastval = ''
+        lasttype = None
+
+        # wrap the whole output with <pre>
+        outfile.write('<div style="font-family: consolas, lucida console, courier, monospace">\n')
+
+        for ttype, value in tokensource:
+            value = value.replace(' ', '&#160;')
+            value = value.replace("<", '&lt;')
+            value = value.replace(">", '&gt;')
+            value = value.replace('\n', '<br/>\n')
+            # if the token type doesn't exist in the stylemap
+            # we try it with the parent of the token type
+            # eg: parent of Token.Literal.String.Double is
+            # Token.Literal.String
+            while ttype not in self.styles:
+                ttype = ttype.parent
+            if ttype == lasttype:
+                # the current token type is the same of the last
+                # iteration. cache it
+                lastval += value
+            else:
+                # not the same token as last iteration, but we
+                # have some data in the buffer. wrap it with the
+                # defined style and write it to the output file
+                if lastval:
+                    stylebegin, styleend = self.styles[lasttype]
+                    outfile.write(stylebegin + lastval + styleend)
+                # set lastval/lasttype to current values
+                lastval = value
+                lasttype = ttype
+
+        # if something is left in the buffer, write it to the
+        # output file, then close the opened <pre> tag
+        if lastval:
+            stylebegin, styleend = self.styles[lasttype]
+            outfile.write(stylebegin + lastval + styleend)
+        outfile.write('</div>\n')
 
 class LexerItem(object):
   def __init__(self, lang_tuple):
@@ -26,7 +102,7 @@ class LexerItem(object):
     return self.longname
 
 lexers = [LexerItem(l) for l in get_all_lexers()]
-lexers.sort()
+lexers.sort(key=lambda l: l.longname)
 styles = list(get_all_styles())
 styles.sort()
 
@@ -161,11 +237,16 @@ def CreateEditor(editorSite):
   return PygmentedCodeEditor(editorSite)
   
 def GeneratePublishHtml(content, publishingContext):
-  Forms.MessageBox.Show(content.Properties["lexer"])
+  Forms.MessageBox.Show("GeneratePublishHtml")
   code = content.Properties["code"]
   try:
     lexer = get_lexer_by_name(content.Properties["lexer"])
   except:
     lexer = get_lexer_by_name("text")
+  
+  if content.Properties["style"]:
+    formatter = DevHawkHtmlFormatter(style=content.Properties["style"])
+  else:
+    formatter = DevHawkHtmlFormatter()
     
-  return highlight(code, lexer, HtmlFormatter())
+  return highlight(code, lexer, formatter)
