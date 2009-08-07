bf1fb3683ddf2d15dbf510c5f793183cd3c1b0fb
Harry Pierson <harry.pierson@microsoft.com>
2009-08-07 15:10:54 -0700

    Modified assembly name to Pygments.WLWriter.DEV in debug mode so it won't collide with installed Pygments.WLWriter.dll

--- register_dev_plugin.ps1
+++ register_dev_plugin.ps1
@@ -1,2 +1,2 @@
-$p = resolve-path Pygments.WLWriter\bin\Debug\Pygments.WLWriter.dll
+$p = resolve-path Pygments.WLWriter\bin\Debug\Pygments.WLWriter.DEV.dll
 new-itemproperty -path "HKCU:\Software\Microsoft\Windows Live\Writer\PluginAssemblies" -name "Pygments.WLWriter.Dev" -value $p
